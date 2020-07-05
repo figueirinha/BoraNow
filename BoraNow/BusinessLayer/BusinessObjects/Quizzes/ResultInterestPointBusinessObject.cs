a731e362c38384f93785049afc759c424309822f
Bruna Costa <brunasmscosta@outlook.com>
2020-07-05 20:23:28 +0100

    Update

--- BoraNow/BusinessLayer/BusinessObjects/Quizzes/ResultInterestPointBusinessObject.cs
+++ BoraNow/BusinessLayer/BusinessObjects/Quizzes/ResultInterestPointBusinessObject.cs
@@ -1,10 +1,267 @@
-﻿using System;
+﻿using Recodme.RD.BoraNow.BusinessLayer.Base;
+using Recodme.RD.BoraNow.BusinessLayer.OperationResults;
+using Recodme.RD.BoraNow.DataAccessLayer.DataAccessObjects.Quizzes;
+using Recodme.RD.BoraNow.DataLayer.Quizzes;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Recodme.RD.BoraNow.BusinessLayer.BusinessObjects.Quizzes
 {
-    class ResultInterestPointBusinessObject
+    public class ResultInterestPointBusinessObject
     {
+
+        private ResultInterestPointDataAccessObject _dao = new ResultInterestPointDataAccessObject();
+        public ResultInterestPointBusinessObject()
+        {
+            _dao = new ResultInterestPointDataAccessObject();
+        }
+
+
+        #region List
+        public OperationResult<List<ResultInterestPoint>> List()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = _dao.List();
+                    ts.Complete();
+                    return new OperationResult<List<ResultInterestPoint>>() { Success = true, Result = result };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<ResultInterestPoint>>() { Success = false, Exception = e };
+            }
+        }
+
+        public async Task<OperationResult<List<ResultInterestPoint>>> ListAsync()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = await _dao.ListAsync();
+                    ts.Complete();
+                    return new OperationResult<List<ResultInterestPoint>>() { Success = true, Result = result };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<ResultInterestPoint>>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Count
+        public OperationResult<int> CountAll()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = _dao.List().Count;
+                    ts.Complete();
+                    return new OperationResult<int>() { Success = true, Result = result };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<int>() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult<int>> CountAllAsync()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = await _dao.ListAsync();
+                    ts.Complete();
+                    return new OperationResult<int>() { Success = true, Result = result.Count };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<int>() { Success = false, Exception = e };
+            }
+        }
+        #endregion 
+
+        #region Create
+        public OperationResult Create(ResultInterestPoint resultInterestPoint)
+        {
+            try
+            {
+
+                _dao.Create(resultInterestPoint);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult> CreateAsync(ResultInterestPoint resultInterestPoint)
+        {
+            try
+            {
+                await _dao.CreateAsync(resultInterestPoint);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Read
+        public OperationResult<ResultInterestPoint> Read(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var res = _dao.Read(id);
+                    transactionScope.Complete();
+                    return new OperationResult<ResultInterestPoint>() { Success = true, Result = res };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<ResultInterestPoint>() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult<ResultInterestPoint>> ReadAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var res = await _dao.ReadAsync(id);
+                    transactionScope.Complete();
+                    return new OperationResult<ResultInterestPoint>() { Success = true, Result = res };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<ResultInterestPoint>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Update
+        public OperationResult Update(ResultInterestPoint resultInterestPoint)
+        {
+            try
+            {
+                _dao.Update(resultInterestPoint);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult> UpdateAsync(ResultInterestPoint resultInterestPoint)
+        {
+            try
+            {
+                await _dao.UpdateAsync(resultInterestPoint);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Delete
+        public OperationResult Delete(ResultInterestPoint resultInterestPoint)
+        {
+            try
+            {
+                _dao.Delete(resultInterestPoint);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        public async Task<OperationResult> DeleteAsync(ResultInterestPoint resultInterestPoint)
+        {
+            try
+            {
+                await _dao.DeleteAsync(resultInterestPoint);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+
+        public OperationResult Delete(Guid id)
+        {
+            try
+            {
+                _dao.Delete(id);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                await _dao.DeleteAsync(id);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+
+        #endregion
     }
 }
