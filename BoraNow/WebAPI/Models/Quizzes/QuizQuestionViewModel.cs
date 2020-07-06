e34fab4e3cc54cbe6a27a03ced468139bbc08df4
figueirinha <figueirinha.mf@gmail.com>
2020-07-06 20:55:48 +0100

    Preperation and organizations for API controllers and view models build 1

---
+++ BoraNow/WebAPI/Models/Quizzes/QuizQuestionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recodme.RD.BoraNow.PresentationLayer.WebAPI.Models.Quizzes
+{
+    public class QuizQuestionViewModel
+    {
+    }
+}
