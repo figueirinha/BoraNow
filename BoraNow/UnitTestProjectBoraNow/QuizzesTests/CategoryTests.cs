83e8e5b2a3ad37e43a8423f4a3df0bbd1879f5df
Bruna Costa <brunasmscosta@outlook.com>
2020-07-09 17:11:28 +0100

    Added UnitTests

---
+++ BoraNow/UnitTestProjectBoraNow/QuizzesTests/CategoryTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recodme.RD.BoraNow.BusinessLayer.BusinessObjects.Quizzes;
+using Recodme.RD.BoraNow.DataLayer.Quizzes;
+using System.Security.Cryptography.Xml;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectBoraNow
+{
+    public class OperationResult { public bool Success { get; set; } }
+
+    [TestClass]
+    public class CategoryTests
+    {
+        [TestMethod]
+        public void TestCreateCategory()
+        {
+            var _category = new Category("Praia");
+            var _bo = new CategoryBusinessObject();
+            _bo.Create(_category);
+            var result = new OperationResult() { Success = true };
+            var _categoryCreated = _bo.Read(_category.Id);
+            Assert.IsTrue(_categoryCreated.Result.Name == _category.Name);
+        }
+        [TestMethod]
+        public void TestUpdateCategory()
+        {
+            var newNameCategory = "Bar";
+            var _bo = new CategoryBusinessObject();
+            var _category = _bo.List().Result[0];
+            _category.Name = newNameCategory;
+            _bo.Update(_category);
+            _category = _bo.List().Result[0];
+            Assert.IsTrue(_category.Name == newNameCategory);
+        }
+        [TestMethod]
+        public void TestDeleteCategoryId()
+        {
+            var _bo = new CategoryBusinessObject();
+            var _category = _bo.List().Result[0];
+            var existingId = _category.Id;
+            _bo.Delete(_category.Id);
+            _category = _bo.List().Result[0];
+            Assert.IsTrue(_category.Id == existingId);
+        }
+
+    }
+}
