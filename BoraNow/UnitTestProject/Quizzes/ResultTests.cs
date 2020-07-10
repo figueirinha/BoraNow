c366377fc575335b5d66a0d3a30bc1ff50d107ed
Bruna Costa <brunasmscosta@outlook.com>
2020-07-10 23:37:35 +0100

    Update Tests in QuizTests, ResultInterstPointTests and ResultTests

--- BoraNow/UnitTestProject/Quizzes/ResultTests.cs
+++ BoraNow/UnitTestProject/Quizzes/ResultTests.cs
@@ -5,27 +5,72 @@ using Recodme.RD.BoraNow.DataAccessLayer.Seeders;
 using Recodme.RD.BoraNow.DataLayer.Quizzes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Recodme.RD.BoraNow.UnitTestProject.Quizzes
 {
     [TestClass]
-
     public class ResultTests
     {
         [TestMethod]
         public void TestCreateResult()
         {
             BoraNowSeeder.Seed();
-            var _boQuiz = new QuizBusinessObject();
-            var _bo = new ResultBusinessObject();
+            var rbo = new ResultBusinessObject();
+            var qbo = new QuizBusinessObject();
 
-            var quiz = new Quiz("Q1");
-            var _result = new Result("Resulta Q1", DateTime.UtcNow, quiz.Id);
-            var resCreate = _bo.Create(_result);
-            var resGet = _bo.Read(_result.Id);
+            var quiz = new Quiz("Quiz 1");
+            qbo.Create(quiz);
+
+            var _result = new Result("Q1 Result", DateTime.UtcNow, quiz.Id);
+
+            var resCreate = rbo.Create(_result);
+            var resGet = rbo.Read(_result.Id);
             Assert.IsTrue(resCreate.Success && resGet.Success && resGet.Result != null);
+        } 
+        
+        [TestMethod]
+        public void TestListResult()
+        {
+            BoraNowSeeder.Seed();
+            var bo = new ResultBusinessObject();
+            var resList = bo.List();
+            Assert.IsTrue(resList.Success && resList.Result.Count == 1);
+        }
+        
+        [TestMethod]
+        public void TestUpdateResult()
+        {
+            BoraNowSeeder.Seed();
+            var rbo = new ResultBusinessObject();
+            var resList = rbo.List();
+            var item = resList.Result.FirstOrDefault();
+
+            var qbo = new QuizBusinessObject();
+            var quiz = new Quiz("Quiz 2");
+            qbo.Create(quiz);
+
+            item.QuizId = quiz.Id;
+            var resUpdate = rbo.Update(item);
+            resList = rbo.List();
+
+            Assert.IsTrue(resList.Success && resUpdate.Success &&
+                resList.Result.First().QuizId == item.QuizId);
 
         }
+
+        [TestMethod]
+        public void TestDeleteResultId()
+        {
+            BoraNowSeeder.Seed();
+            var bo = new ResultBusinessObject();
+            var resList = bo.List();
+            var resDelete = bo.Delete(resList.Result.First().Id);
+            resList = bo.List();
+
+            Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
+        }
+
     }
 }
