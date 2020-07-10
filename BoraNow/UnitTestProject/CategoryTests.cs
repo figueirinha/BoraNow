a51eebf7ca7b772a5758982591446abc6a357a9c
Bruna Costa <brunasmscosta@outlook.com>
2020-07-10 16:37:39 +0100

    Update Tests

--- BoraNow/UnitTestProject/CategoryTests.cs
+++ BoraNow/UnitTestProject/CategoryTests.cs
@@ -1,13 +1,43 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recodme.RD.BoraNow.BusinessLayer.BusinessObjects.Quizzes;
+using Recodme.RD.BoraNow.DataLayer.Quizzes;
 
 namespace UnitTestProject
 {
+
     [TestClass]
-    public class UnitTest1
+    public class CategoryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCreateCategory()
         {
+            var _category = new Category("Praia");
+            var _bo = new CategoryBusinessObject();
+            _bo.Create(_category);
+            var _categoryCreated = _bo.Read(_category.Id);
+            Assert.IsTrue(_categoryCreated.Result.Name == _category.Name);
         }
+        [TestMethod]
+        public void TestUpdateCategory()
+        {
+            var newNameCategory = "Bar";
+            var _bo = new CategoryBusinessObject();
+            var _category = _bo.List().Result[0];
+            _category.Name = newNameCategory;
+            _bo.Update(_category);
+            _category = _bo.List().Result[0];
+            Assert.IsTrue(_category.Name == newNameCategory);
+        }
+        [TestMethod]
+        public void TestDeleteCategoryId()
+        {
+            var _bo = new CategoryBusinessObject();
+            var _category = _bo.List().Result[0];
+            var existingId = _category.Id;
+            _bo.Delete(_category.Id);
+            _category = _bo.List().Result[0];
+            Assert.IsTrue(_category.Id == existingId);
+        }
+
     }
 }
--- BoraNow/UnitTestProject/QuizTests.cs
+++ BoraNow/UnitTestProject/QuizTests.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recodme.RD.BoraNow.BusinessLayer.BusinessObjects.Quizzes;
+using Recodme.RD.BoraNow.DataLayer.Quizzes;
 
 namespace UnitTestProject
 {
-    class QuizTests
+
+    [TestClass]
+    public class QuizTests
     {
+        [TestMethod]
+        public void TestCreateQuiz()
+        {
+            var _Quiz = new Quiz("Questionário BoraNow");
+            var _bo = new QuizBusinessObject();
+            _bo.Create(_Quiz);
+            var _QuizCreated = _bo.Read(_Quiz.Id);
+            Assert.IsTrue(_QuizCreated.Result.Title == _Quiz.Title);
+        }
+        [TestMethod]
+        public void TestUpdateQuiz()
+        {
+            var newTitleQuiz = "Quiz Bora Now";
+            var _bo = new QuizBusinessObject();
+            var _Quiz = _bo.List().Result[0];
+            _Quiz.Title = newTitleQuiz;
+            _bo.Update(_Quiz);
+            _Quiz = _bo.List().Result[0];
+            Assert.IsTrue(_Quiz.Title == newTitleQuiz);
+        }
+        [TestMethod]
+        public void TestDeleteQuizId()
+        {
+            var _bo = new QuizBusinessObject();
+            var _Quiz = _bo.List().Result[0];
+            var existingId = _Quiz.Id;
+            _bo.Delete(_Quiz.Id);
+            _Quiz = _bo.List().Result[0];
+            Assert.IsTrue(_Quiz.Id == existingId);
+        }
+
     }
 }
