6b20b81628db9757a5b47d57e2d10594b339dc0b
figueirinha <figueirinha.mf@gmail.com>
2020-07-16 00:18:44 +0100

    Web API folder for build 3 added

--- BoraNow/BusinessLayer/BusinessObjects/Users/ProfileBusinessObject.cs
+++ BoraNow/BusinessLayer/BusinessObjects/Users/ProfileBusinessObject.cs
@@ -1,10 +1,264 @@
-﻿using System;
+﻿using Recodme.RD.BoraNow.BusinessLayer.OperationResults;
+using Recodme.RD.BoraNow.DataAccessLayer.DataAccessObjects.Users;
+using Recodme.RD.BoraNow.DataLayer.Users;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Recodme.RD.BoraNow.BusinessLayer.BusinessObjects.Users
 {
-    class ProfileBusinessObject
+    public class ProfileBusinessObject
     {
+        private ProfileDataAccessObject _dao;
+        public ProfileBusinessObject()
+        {
+            _dao = new ProfileDataAccessObject();
+        }
+
+        #region List
+        public OperationResult<List<Profile>> List()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = _dao.List();
+                    ts.Complete();
+                    return new OperationResult<List<Profile>>() { Success = true, Result = result };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<Profile>>() { Success = false, Exception = e };
+            }
+        }
+
+        public async Task<OperationResult<List<Profile>>> ListAsync()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = await _dao.ListAsync();
+                    ts.Complete();
+                    return new OperationResult<List<Profile>>() { Success = true, Result = result };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<Profile>>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Count
+        public OperationResult<int> CountAll()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = _dao.List().Count;
+                    ts.Complete();
+                    return new OperationResult<int>() { Success = true, Result = result };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<int>() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult<int>> CountAllAsync()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var ts = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var result = await _dao.ListAsync();
+                    ts.Complete();
+                    return new OperationResult<int>() { Success = true, Result = result.Count };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<int>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Create
+        public OperationResult Create(Profile profile)
+        {
+            try
+            {
+
+                _dao.Create(profile);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult> CreateAsync(Profile profile)
+        {
+            try
+            {
+                await _dao.CreateAsync(profile);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Read
+        public OperationResult<Profile> Read(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var res = _dao.Read(id);
+                    transactionScope.Complete();
+                    return new OperationResult<Profile>() { Success = true, Result = res };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<Profile>() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult<Profile>> ReadAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var res = await _dao.ReadAsync(id);
+                    transactionScope.Complete();
+                    return new OperationResult<Profile>() { Success = true, Result = res };
+                }
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<Profile>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Update
+        public OperationResult Update(Profile profile)
+        {
+            try
+            {
+                _dao.Update(profile);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        public async Task<OperationResult> UpdateAsync(Profile profile)
+        {
+            try
+            {
+                await _dao.UpdateAsync(profile);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Delete
+        public OperationResult Delete(Profile profile)
+        {
+            try
+            {
+                _dao.Delete(profile);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        public async Task<OperationResult> DeleteAsync(Profile profile)
+        {
+            try
+            {
+                await _dao.DeleteAsync(profile);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+
+        public OperationResult Delete(Guid id)
+        {
+            try
+            {
+                _dao.Delete(id);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                await _dao.DeleteAsync(id);
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = true, Exception = e };
+            }
+        }
+        #endregion
     }
 }
