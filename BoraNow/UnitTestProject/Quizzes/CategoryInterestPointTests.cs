72627e22bf8387f33e2829940d06c9d8193b1c7f
Bruna Costa <brunasmscosta@outlook.com>
2020-07-21 19:27:23 +0100

    .

---
+++ BoraNow/UnitTestProject/Quizzes/CategoryInterestPointTests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recodme.RD.BoraNow.BusinessLayer.BusinessObjects.Quizzes;
+using Recodme.RD.BoraNow.DataAccessLayer.Seeders;
+using Recodme.RD.BoraNow.DataLayer.Quizzes;
+using System.Linq;
+
+
+namespace Recodme.RD.BoraNow.UnitTestProject.Quizzes
+{
+    [TestClass]
+    public class CategoryInterestPointTests
+    {
+        [TestMethod]
+        public void TestCreateCategoryInterestPoint()
+        {
+            BoraNowSeeder.Seed();
+            var cipbo = new CategoryInterestPointBusinessObject();
+
+            var categoryInterestPoint= new CategoryInterestPoint("B");
+
+            var resCreate = cipbo.Create(categoryInterestPoint);
+            var resGet = cipbo.Read(categoryInterestPoint.Id);
+
+            Assert.IsTrue(resGet.Success && resCreate.Success && resGet.Result != null);
+        }
+
+        [TestMethod]
+        public void TestCreateCategoryInterestPointAsync()
+        {
+            BoraNowSeeder.Seed();
+            var cipbo = new CategoryInterestPointBusinessObject();
+
+            var categoryInterestPoint = new CategoryInterestPoint("B");
+ 
+
+            var resCreate = cipbo.CreateAsync(categoryInterestPoint).Result;
+            var resGet = cipbo.ReadAsync(categoryInterestPoint.Id).Result;
+
+            Assert.IsTrue(resGet.Success && resCreate.Success && resGet.Result != null);
+        }
+
+        [TestMethod]
+        public void TestListCategoryInterestPoint()
+        {
+            BoraNowSeeder.Seed();
+            var cipbo = new CategoryInterestPointBusinessObject();
+            var resList = cipbo.List();
+
+            Assert.IsTrue(resList.Success && resList.Result.Count == 1);
+
+        }
+
+        [TestMethod]
+        public void TestListCategoryInterestPointAsync()
+        {
+            BoraNowSeeder.Seed();
+            var cipbo = new CategoryInterestPointBusinessObject();
+            var resList = cipbo.ListAsync().Result;
+
+            Assert.IsTrue(resList.Success && resList.Result.Count == 1);
+
+        }
+
+        [TestMethod]
+        public void TestUpdatetCategoryInterestPoint()
+        {
+            BoraNowSeeder.Seed();
+            var cipbo = new CategoryInterestPointBusinessObject();
+            var resList = cipbo.List();
+            var item = resList.Result.FirstOrDefault();
+
+   
+            var categoryInterestPoint = new CategoryInterestPoint("C");
+
+            item.Name = categoryInterestPoint.Name;
+            var resUpdate = cipbo.Update(item);
+            resList = cipbo.List();
+
+            Assert.IsTrue(resUpdate.Success && resList.Success && resList.Result.First().Name == categoryInterestPoint.Name);
+
+        }
+
+        [TestMethod]
+        public void TestUpdateInterestpointCategoryAsync()
+        {
+            BoraNowSeeder.Seed();
+            var cipbo = new CategoryInterestPointBusinessObject();
+            var resList = cipbo.List();
+            var item = resList.Result.FirstOrDefault();
+
+            var categoryInterestPoint = new CategoryInterestPoint("C");
+
+            item.Name = categoryInterestPoint.Name;
+            var resUpdate = cipbo.UpdateAsync(item).Result;
+            resList = cipbo.ListAsync().Result;
+
+            Assert.IsTrue(resUpdate.Success && resList.Success && resList.Result.First().Name == categoryInterestPoint.Name);
+        }
+
+        [TestMethod]
+        public void TestDeleteInterestPointCategory()
+        {
+            BoraNowSeeder.Seed();
+            var bo = new CategoryInterestPointBusinessObject();
+            var resList = bo.List();
+            var resDelete = bo.Delete(resList.Result.First().Id);
+            resList = bo.List();
+
+            Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
+
+        }
+
+
+        [TestMethod]
+        public void TestDeleteInterestPointCategoryAsync()
+        {
+            BoraNowSeeder.Seed();
+            var bo = new CategoryInterestPointBusinessObject();
+            var resList = bo.List();
+            var resDelete = bo.DeleteAsync(resList.Result.First().Id).Result;
+            resList = bo.ListAsync().Result;
+
+            Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
+        }
+    }
+}
--- BoraNow/UnitTestProject/Quizzes/QuizAnswerTests.cs
+++ BoraNow/UnitTestProject/Quizzes/QuizAnswerTests.cs
@@ -9,151 +9,151 @@ using System.Text;
 
 namespace Recodme.RD.BoraNow.UnitTestProject.Quizzes
 {
-    [TestClass]
+   // [TestClass]
 
-    public class QuizAnswerTests
-    {
-        [TestMethod]
-        public void TestCreateQuizAnswer()
-        {
-            BoraNowSeeder.Seed();
-            var qabo = new QuizAnswerBusinessObject();
-            var qqbo = new QuizQuestionBusinessObject();
-            var quizbo = new QuizBusinessObject();
+    //public class QuizAnswerTests
+    //{
+    //    [TestMethod]
+    //    public void TestCreateQuizAnswer()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var qabo = new QuizAnswerBusinessObject();
+    //        var qqbo = new QuizQuestionBusinessObject();
+    //        var quizbo = new QuizBusinessObject();
 
-            var newQuiz = new Quiz("Where you wanna go?");
-            var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
+    //        var newQuiz = new Quiz("Where you wanna go?");
+    //        var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
 
-            quizbo.Create(newQuiz);
-            qqbo.Create(newQuizQuestion);
+    //        quizbo.Create(newQuiz);
+    //        qqbo.Create(newQuizQuestion);
 
-            var newQuizAnswer = new QuizAnswer("Beach", newQuizQuestion.Id);
+    //        var newQuizAnswer = new QuizAnswer("Beach", newQuizQuestion.Id);
 
-            var resCreate = qabo.Create(newQuizAnswer);
-            var resGet = qabo.Read(newQuizAnswer.Id);           
+    //        var resCreate = qabo.Create(newQuizAnswer);
+    //        var resGet = qabo.Read(newQuizAnswer.Id);           
 
-            Assert.IsTrue(resCreate.Success && resGet.Success && resGet.Result != null);
-        }
+    //        Assert.IsTrue(resCreate.Success && resGet.Success && resGet.Result != null);
+    //    }
 
-        [TestMethod]
-        public void TestCreateQuizAnswerAsync()
-        {
-            BoraNowSeeder.Seed();
-            var qabo = new QuizAnswerBusinessObject();
-            var qqbo = new QuizQuestionBusinessObject();
-            var quizbo = new QuizBusinessObject();
+    //    [TestMethod]
+    //    public void TestCreateQuizAnswerAsync()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var qabo = new QuizAnswerBusinessObject();
+    //        var qqbo = new QuizQuestionBusinessObject();
+    //        var quizbo = new QuizBusinessObject();
 
-            var newQuiz = new Quiz("Where you wanna go?");
-            var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
+    //        var newQuiz = new Quiz("Where you wanna go?");
+    //        var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
 
-            quizbo.Create(newQuiz);
-            qqbo.Create(newQuizQuestion);
+    //        quizbo.Create(newQuiz);
+    //        qqbo.Create(newQuizQuestion);
 
-            var newQuizAnswer = new QuizAnswer("Beach", newQuizQuestion.Id);
+    //        var newQuizAnswer = new QuizAnswer("Beach", newQuizQuestion.Id);
 
-            var resCreate = qabo.CreateAsync(newQuizAnswer).Result;
-            var resGet = qabo.ReadAsync(newQuizAnswer.Id).Result;
+    //        var resCreate = qabo.CreateAsync(newQuizAnswer).Result;
+    //        var resGet = qabo.ReadAsync(newQuizAnswer.Id).Result;
 
-            Assert.IsTrue(resCreate.Success && resGet.Success && resGet.Result != null);
-        }
+    //        Assert.IsTrue(resCreate.Success && resGet.Success && resGet.Result != null);
+    //    }
 
-        [TestMethod]
-        public void TestListQuizAnswer()
-        {
-            BoraNowSeeder.Seed();
-            var bo = new QuizAnswerBusinessObject();
-            var resList = bo.List();
+    //    [TestMethod]
+    //    public void TestListQuizAnswer()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var bo = new QuizAnswerBusinessObject();
+    //        var resList = bo.List();
 
-            Assert.IsTrue(resList.Success && resList.Result.Count == 1);
-        }
+    //        Assert.IsTrue(resList.Success && resList.Result.Count == 1);
+    //    }
 
-        [TestMethod]
-        public void TestListQuizAnswerAsync()
-        {
-            BoraNowSeeder.Seed();
-            var bo = new QuizAnswerBusinessObject();
-            var resList = bo.ListAsync().Result;
+    //    [TestMethod]
+    //    public void TestListQuizAnswerAsync()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var bo = new QuizAnswerBusinessObject();
+    //        var resList = bo.ListAsync().Result;
 
-            Assert.IsTrue(resList.Success && resList.Result.Count == 1);
-        }
+    //        Assert.IsTrue(resList.Success && resList.Result.Count == 1);
+    //    }
 
-        [TestMethod]
-        public void TestUpdateQuizAnswer()
-        {
-            BoraNowSeeder.Seed();
-            var qabo = new QuizAnswerBusinessObject();
-            var qqbo = new QuizQuestionBusinessObject();
-            var quizbo= new QuizBusinessObject();
-            var resList = qabo.List();
-            var quizAnswer = resList.Result.FirstOrDefault();
-
-
-            var newQuiz = new Quiz("Where you wanna go?");
-            var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
-
-            quizbo.Create(newQuiz);
-            qqbo.Create(newQuizQuestion);
-
-            quizAnswer.QuizQuestionId = newQuizQuestion.Id;
-            quizAnswer.Answer = "yes";
-
-            var resUpdate= qabo.Update(quizAnswer);
-            resList = qabo.List();
-
-            Assert.IsTrue(resUpdate.Success && resList.Success && resList.Result.First().Answer == "yes"
-                && resList.Result.First().QuizQuestionId == newQuizQuestion.Id);
-        }
-
-        [TestMethod]
-        public void TestUpdateQuizAnswerAsync()
-        {
-            BoraNowSeeder.Seed();
-            var qabo = new QuizAnswerBusinessObject();
-            var qqbo = new QuizQuestionBusinessObject();
-            var quizbo = new QuizBusinessObject();
-            var resList = qabo.List();
-            var quizAnswer = resList.Result.FirstOrDefault();
+    //    [TestMethod]
+    //    public void TestUpdateQuizAnswer()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var qabo = new QuizAnswerBusinessObject();
+    //        var qqbo = new QuizQuestionBusinessObject();
+    //        var quizbo= new QuizBusinessObject();
+    //        var resList = qabo.List();
+    //        var quizAnswer = resList.Result.FirstOrDefault();
 
 
-            var newQuiz = new Quiz("Where you wanna go?");
-            var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
+    //        var newQuiz = new Quiz("Where you wanna go?");
+    //        var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
 
-            quizbo.Create(newQuiz);
-            qqbo.Create(newQuizQuestion);
+    //        quizbo.Create(newQuiz);
+    //        qqbo.Create(newQuizQuestion);
 
-            quizAnswer.QuizQuestionId = newQuizQuestion.Id;
-            quizAnswer.Answer = "yes";
+    //        quizAnswer.QuizQuestionId = newQuizQuestion.Id;
+    //        quizAnswer.Answer = "yes";
 
-            var resUpdate = qabo.UpdateAsync(quizAnswer).Result;
-            resList = qabo.ListAsync().Result;
+    //        var resUpdate= qabo.Update(quizAnswer);
+    //        resList = qabo.List();
 
-            Assert.IsTrue(resUpdate.Success && resList.Success && resList.Result.First().Answer == "yes"
-                && resList.Result.First().QuizQuestionId == newQuizQuestion.Id);
-        }
+    //        Assert.IsTrue(resUpdate.Success && resList.Success && resList.Result.First().Answer == "yes"
+    //            && resList.Result.First().QuizQuestionId == newQuizQuestion.Id);
+    //    }
 
-        [TestMethod]
-        public void TestDeleteQuizAnswer()
-        {
-            BoraNowSeeder.Seed();
-            var bo = new QuizAnswerBusinessObject();
-            var resList = bo.List();
-            var resDelete = bo.Delete(resList.Result.First().Id);
-            resList = bo.List();
-
-            Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
-        }
+    //    [TestMethod]
+    //    public void TestUpdateQuizAnswerAsync()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var qabo = new QuizAnswerBusinessObject();
+    //        var qqbo = new QuizQuestionBusinessObject();
+    //        var quizbo = new QuizBusinessObject();
+    //        var resList = qabo.List();
+    //        var quizAnswer = resList.Result.FirstOrDefault();
 
 
-        [TestMethod]
-        public void TestDeleteQuizAnswerAsync()
-        {
-            BoraNowSeeder.Seed();
-            var bo = new QuizAnswerBusinessObject();
-            var resList = bo.List();
-            var resDelete = bo.DeleteAsync(resList.Result.First().Id).Result;
-            resList = bo.ListAsync().Result;
+    //        var newQuiz = new Quiz("Where you wanna go?");
+    //        var newQuizQuestion = new QuizQuestion("Where you wanna go?", newQuiz.Id);
 
-            Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
-        }
-    }
+    //        quizbo.Create(newQuiz);
+    //        qqbo.Create(newQuizQuestion);
+
+    //        quizAnswer.QuizQuestionId = newQuizQuestion.Id;
+    //        quizAnswer.Answer = "yes";
+
+    //        var resUpdate = qabo.UpdateAsync(quizAnswer).Result;
+    //        resList = qabo.ListAsync().Result;
+
+    //        Assert.IsTrue(resUpdate.Success && resList.Success && resList.Result.First().Answer == "yes"
+    //            && resList.Result.First().QuizQuestionId == newQuizQuestion.Id);
+    //    }
+
+    //    [TestMethod]
+    //    public void TestDeleteQuizAnswer()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var bo = new QuizAnswerBusinessObject();
+    //        var resList = bo.List();
+    //        var resDelete = bo.Delete(resList.Result.First().Id);
+    //        resList = bo.List();
+
+    //        Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
+    //    }
+
+
+    //    [TestMethod]
+    //    public void TestDeleteQuizAnswerAsync()
+    //    {
+    //        BoraNowSeeder.Seed();
+    //        var bo = new QuizAnswerBusinessObject();
+    //        var resList = bo.List();
+    //        var resDelete = bo.DeleteAsync(resList.Result.First().Id).Result;
+    //        resList = bo.ListAsync().Result;
+
+    //        Assert.IsTrue(resDelete.Success && resList.Success && resList.Result.First().IsDeleted);
+    //    }
+    //}
 }
