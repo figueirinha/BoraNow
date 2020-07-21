f967f2081fb33fc11c54c0b6b26b4185fcebc2e4
figueirinha <figueirinha.mf@gmail.com>
2020-07-21 19:43:20 +0100

    aaaaaaaaaa

---
+++ BoraNow/WebAPI/Controllers/Web/QuizzesControllers/InterestPointsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recodme.RD.BoraNow.PresentationLayer.WebAPI.Controllers.Web.QuizzesControllers
+{
+    public class InterestPointsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
