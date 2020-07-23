5b28bbc6a6b7ccf5bd94d09014d187e502120069
Marco Pereira <m.pereira@recodme.pt>
2020-07-23 11:52:27 +0100

    quizresults controller updated

--- BoraNow/WebAPI/Models/Quizzes/QuizAnswerViewModel.cs
+++ BoraNow/WebAPI/Models/Quizzes/QuizAnswerViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Recodme.RD.BoraNow.DataLayer.Quizzes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace Recodme.RD.BoraNow.PresentationLayer.WebAPI.Models.Quizzes
     {
         public Guid Id { get; set; }
         public string Answer { get; set; }
+
+        [Display(Name = "Question")]
         public Guid QuizQuestionId { get; set; }
 
         public QuizAnswer ToQuizAnswer()
